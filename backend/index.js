1e88ccf705dedab3e5136c56fc36f71e734b0c95
sushil <sushilbajpai2003@gmail.com>
2023-07-14 09:00:06 +0530

    done

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const PORT=process.env.PORT || 5001
-require("dotenv").config()
-
-const app=require("./app");
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
-
